2cc58a8c38dc0bc30f977cb998c98abbbcf5f56c
Jack Fraser <jdfras06@gmail.com>
2024-06-13 10:39:55 +0930

    Add Mapbox-Unity-SDK to project.

---
+++ Assets/CLLMM/Scripts/MapOverlayManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace CLLMM.Scripts
+{
+    public class MapOverlayManager : MonoBehaviour
+    {
+        [SerializeField] private Camera _overlayProjectorCamera;
+        [SerializeField] private GameObject _overlayTouchscreenUI;
+        [SerializeField] private bool _showOnStart;
+        
+        private void Start()
+        {
+            if (_showOnStart)
+            {
+                ShowMapOverlay();
+            }
+            else
+            {
+                HideMapOverlay();
+            }
+        }
+
+        public void ShowMapOverlay()
+        {
+            _overlayProjectorCamera.gameObject.SetActive(true);
+            _overlayTouchscreenUI.gameObject.SetActive(true);
+        }
+
+        public void HideMapOverlay()
+        {
+            _overlayProjectorCamera.gameObject.SetActive(false);
+            _overlayTouchscreenUI.gameObject.SetActive(false);
+        }
+    }
+}
